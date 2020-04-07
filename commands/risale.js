3d9fca8fce434160b6d0c448bb0287e43f515fd2
Selami Cetinguney <selami.c92@hotmail.com>
2020-04-07 20:58:52 +0200

    added the text value of the found links

--- commands/risale.js
+++ commands/risale.js
@@ -9,7 +9,7 @@ const COMMAND_NAME = 'zoekrisale';
 module.exports = {
     active: true,
     name: COMMAND_NAME,
-    description: 'Zoek naar stukken in de Risale-i Nur.',
+    description: 'Zoek naar stukken tekst uit de Risale-i Nur.',
     args: true,
     minArgs: 1,
     usages: [
@@ -29,9 +29,9 @@ module.exports = {
 
         const root = HTMLParser.parse(data);
 
-        const foundElement = root.querySelector('[target="_blank"]');
+        const foundElements = root.querySelectorAll('[data-id]');
 
-        if (!foundElement) {
+        if (!foundElements.length) {
             const embed = setEmbed({
                 title: `Geen resultaten gevonden voor \`${searchQuery}\`.`
             });
@@ -39,14 +39,23 @@ module.exports = {
             return message.channel.send(embed);
         }
 
+        let fields = [];
+        foundElements.forEach(element => {
+            const title = element.firstChild;
+            const text  = element.lastChild.innerHTML
+                          .replace(/<b>|<\/b>/g, '**')
+                          .replace(/\r\n/g, ' ');
+
+            fields.push({
+                name: `**${title.innerHTML.split(' | ')[0]}**\n${title.innerHTML.split(' | ')[1]}:`,
+                value: `[${text}](${title.getAttribute('href')})`
+            });
+        });
+
         const embed = setEmbed({
             title: `Gezocht naar \`${searchQuery}\`.`,
-            fields: [
-                {
-                    name: 'Lees online as PDF',
-                    value: `[${foundElement.innerHTML}](${foundElement.getAttribute('href')})`
-                }
-            ],
+            description: 'Klik op een link om de juiste pagina in PDF te openen.',
+            fields,
             message
         });
 
